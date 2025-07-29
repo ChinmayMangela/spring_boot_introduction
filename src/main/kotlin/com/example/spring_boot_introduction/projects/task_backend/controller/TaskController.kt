9d0af85c728e4fce04dba91af88e43afd783e4dc
apple <mangelachinmay7@gmail.com>
2025-07-29 23:08:19 +0530

    Filter tasks by priority
    This commit introduces the ability to filter tasks by priority in the `TaskController`.
    
    The `getAllTasks` endpoint now accepts an optional query parameter `q` of type `TaskPriority`. If the `q` parameter is provided, the endpoint will return a list of tasks filtered by the specified priority. If `q` is not provided, all tasks will be returned.
    
    The `@PathVariable` annotations in `getTask`, `updateTask`, and `deleteTask` have been updated to explicitly specify the path variable name as "id" using `@PathVariable("id")`.

--- src/main/kotlin/com/example/spring_boot_introduction/projects/task_backend/controller/TaskController.kt
+++ src/main/kotlin/com/example/spring_boot_introduction/projects/task_backend/controller/TaskController.kt
@@ -1,7 +1,7 @@
 package com.example.spring_boot_introduction.projects.task_backend.controller
-
 import com.example.spring_boot_introduction.projects.task_backend.dto.TaskRequest
 import com.example.spring_boot_introduction.projects.task_backend.dto.TaskResponse
+import com.example.spring_boot_introduction.projects.task_backend.model.TaskPriority
 import com.example.spring_boot_introduction.projects.task_backend.service.TaskService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -11,26 +11,29 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController()
 @RequestMapping("/tasks")
 class TaskController(
-    private val taskService: TaskService
+    private val taskService: TaskService,
 ) {
     @PostMapping
     fun addTask(
-        @RequestBody taskRequest: TaskRequest
+        @RequestBody taskRequest: TaskRequest,
     ): ResponseEntity<TaskResponse> {
         val task = taskService.addTask(taskRequest)
         return ResponseEntity.ok(task)
     }
 
     @GetMapping("/{id}")
-    fun getTask(@PathVariable id: String): ResponseEntity<TaskResponse> {
+    fun getTask(
+        @PathVariable("id") id: String,
+    ): ResponseEntity<TaskResponse> {
         val task = taskService.getTask(id)
-        return if(task != null) {
+        return if (task != null) {
             ResponseEntity.ok(task)
         } else {
             ResponseEntity.notFound().build()
@@ -38,15 +41,26 @@ class TaskController(
     }
 
     @GetMapping
-    fun getAllTasks(): ResponseEntity<List<TaskResponse>> {
+    fun getAllTasks(
+        @RequestParam("q", required = false) query: TaskPriority?
+    ): ResponseEntity<List<TaskResponse>> {
         val tasks = taskService.getAllTasks()
-        return ResponseEntity.ok(tasks)
+        return if (query == null) {
+            ResponseEntity.ok(tasks)
+        } else {
+            val filteredTasks = tasks.filter { it.priority == query }
+            ResponseEntity.ok(filteredTasks)
+        }
     }
 
+
     @PutMapping("/{id}")
-    fun updateTask(@PathVariable id: String, @RequestBody task: TaskRequest): ResponseEntity<TaskResponse> {
+    fun updateTask(
+        @PathVariable("id") id: String,
+        @RequestBody task: TaskRequest,
+    ): ResponseEntity<TaskResponse> {
         val task = taskService.updateTask(id, task)
-        return if(task != null) {
+        return if (task != null) {
             ResponseEntity.ok(task)
         } else {
             ResponseEntity.notFound().build()
@@ -55,10 +69,10 @@ class TaskController(
 
     @DeleteMapping("/{id}")
     fun deleteTask(
-        @PathVariable id: String,
+        @PathVariable("id") id: String,
     ): ResponseEntity<TaskResponse> {
         val deletedTask = taskService.deleteTask(id)
-        return if(deletedTask != null) {
+        return if (deletedTask != null) {
             ResponseEntity.ok(deletedTask)
         } else {
             ResponseEntity.notFound().build()
